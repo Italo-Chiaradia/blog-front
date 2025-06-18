5f807876a758892a604b4aa02e44b92abce97560
Italo Chiaradia <italocoelho050704@gmail.com>
2025-06-18 11:05:30 -0300

    create Posts component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { MainLayoutComponent } from './layouts/main-layout/main-layout.component
 export const routes: Routes = [
   {
     path: '',
-    component: MainLayoutComponent
+    component: MainLayoutComponent,
+    loadChildren: () => import('./pages/public.routes').then(c => c.publicRoutes)
   }
 ];
